2b65129b5c47ebeeb2d537ed2a76e05f7d9b9df4
Mae <funj451@gmail.com>
2023-05-26 11:06:50 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# REST
+
+- RESTful HTTP API
+- Representational State Transfer, aka REST
+- Architectural style for building scalable web apps
+
+URL verb functionality
+"/notes" GET > fetches all resources
+"/notes/10" GET > fetches single resource
+"/notes" POST > create a new resource
+"/notes/10" DELETE > deletes single resource
+"/notes/10" PUT > updates single resource
+"/notes/10" PATCH > replace single resource
---
+++ index.js
@@ -0,0 +1,142 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: "Browser can execute only Javascript",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "CSS is hard",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "Server programming is cool",
+    important: true,
+  },
+];
+
+let persons = [
+  {
+    id: 1,
+    name: "Ellen Mae Nerosa",
+    number: "+63950-6101-901",
+  },
+  {
+    id: 2,
+    name: "Juan Dela Cruz",
+    number: "+63909-2453-909",
+  },
+  {
+    id: 3,
+    name: "Ramon Gonzales",
+    number: "+63906-768-768",
+  },
+
+  {
+    id: 4,
+    name: "Carlo Dacuyan",
+    number: "+63996-234-890",
+  },
+];
+
+function generateId(db) {
+  const maxId = db.length > 0 ? Math.max(...db.map((d) => d.id)) : 0;
+
+  return maxId + 1;
+}
+app.get("/", (request, response) => {
+  response.send("<h1>Hello, Nodemon!</h1>");
+});
+app.get("/api/notes", (request, response) => {
+  response.status(200).json(notes);
+});
+app.get("/api/notes/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  response.status(200).json(note);
+});
+app.delete("/api/notes/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(notes),
+  };
+
+  notes = notes.concat(note);
+
+  response.status(201).json(note);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.status(200).json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  response.status(200).json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const { name, number } = request.body;
+
+  if (!name || !number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  const nameExists = persons.some((person) => person.name === name);
+
+  if (nameExists) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+  const person = {
+    name,
+    number,
+    id: generateId(persons),
+  };
+
+  persons = persons.concat(person);
+  response.status(201).json(person);
+});
+
+app.get("/info", (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people<p>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is now running on Port ${PORT}`);
+});
